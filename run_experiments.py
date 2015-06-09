9111c619eff0ca357c4ee7aa3ef321f3102bb148
matthewheston <matthewheston@gmail.com>
2015-06-09 17:26:13 -0500

    Update experiments script for grid search, top features, prediciton, etc.

--- run_experiments.py
+++ run_experiments.py
@@ -4,10 +4,13 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.cross_validation import cross_val_score, KFold
 from sklearn.pipeline import Pipeline
+from sklearn.metrics import f1_score
+from sklearn.grid_search import GridSearchCV
 import pandas as pd
 import numpy as np
+import sys
 
-yaks = pd.read_csv("data/northwestern_yaks_partitioned.csv", escapechar="\\")
+yaks = pd.read_csv("northwestern_yaks_partitioned.csv", escapechar="\\", quotechar='"', error_bad_lines=False)
 
 messages = yaks["message"]
 
@@ -32,7 +35,7 @@ def get_features(yak, vectorizer, feature_names):
 def report_model(clf, clfname, ngrams=1, features_to_include = []):
     y = yaks["score"].values.astype(np.float32)
     vect = DictVectorizer()
-    c_vectorizer = CountVectorizer(binary=True, ngram_range=(1,2)).build_tokenizer()
+    c_vectorizer = CountVectorizer(binary=True, ngram_range=(1,ngrams)).build_analyzer()
 
     # Create temporary version of features that can be passed to get_features
     temp = zip(*[list(yaks[x]) for x in ['message'] + features_to_include])
@@ -50,14 +53,116 @@ def main():
     discretize_scores()
     temporal_features = ['time_of_day', 'day_of_week']
     text_features = ['message_len', 'num_words', 'mean_word_len']
-    logit_clf = LogisticRegression(penalty='l2', class_weight="auto")
+    logit_clf = LogisticRegression(penalty='l2', C=0.1, class_weight="auto")
     report_model(logit_clf, "Logistic Regression with l2 penalization")
-    report_model(logit_clf, "Logistic Regression with l2 penalization", ngrams=2)
+    report_model(logit_clf, "Logistic Regression with l2 penalization bigrams", ngrams=2)
     report_model(logit_clf, "Logistic Regression with l2 penalization, with features", ngrams=2, features_to_include = temporal_features + text_features)
     nb_clf = MultinomialNB()
     report_model(nb_clf, "Naive Bayes")
-    report_model(nb_clf, "Naive Bayes", ngrams=2)
+    report_model(nb_clf, "Naive Bayes bigrams", ngrams=2)
     report_model(nb_clf, "Naive Bayes with features", ngrams=2, features_to_include = temporal_features + text_features)
 
+def grid_search():
+    discretize_scores()
+
+    # set up grid search
+    parameters = [{"penalty": ["l1", "l2"], "C": [0.01, 0.1, 1.0, 100.0]}]
+    clf = GridSearchCV(LogisticRegression(class_weight="auto"), parameters, "f1", cv=5, n_jobs = int(sys.argv[2])) # get number of parallel jobs from command line args
+
+    # create feature matrix
+    temporal_features = ['time_of_day', 'day_of_week']
+    text_features = ['message_len', 'num_words', 'mean_word_len']
+    features_to_include = temporal_features + text_features
+    y = yaks["score"].values.astype(np.float32)
+    vect = DictVectorizer()
+    c_vectorizer = CountVectorizer(binary=True, ngram_range=(1,2)).build_analyzer()
+    temp = zip(*[list(yaks[x]) for x in ['message'] + features_to_include])
+    tokenized = map(lambda x: get_features(x, c_vectorizer, features_to_include), temp)
+    X = vect.fit_transform(tokenized)
+
+    # run it and print results
+    clf.fit(X, y)
+    print "Best parameters set found on development set:"
+    print
+    print clf.best_estimator_
+    print
+    print "Grid scores on development set:"
+    print
+    for params, mean_score, scores in clf.grid_scores_:
+        print "%0.3f (+/-%0.03f) for %r" % (
+            mean_score, scores.std() / 2, params)
+
+def print_top_results():
+    discretize_scores()
+
+    # create feature matrix
+    temporal_features = ['time_of_day', 'day_of_week']
+    text_features = ['message_len', 'num_words', 'mean_word_len']
+    features_to_include = temporal_features + text_features
+    y = yaks["score"].values.astype(np.float32)
+    vect = DictVectorizer()
+    c_vectorizer = CountVectorizer(binary=True, ngram_range=(1,2)).build_analyzer()
+    temp = zip(*[list(yaks[x]) for x in ['message'] + features_to_include])
+    tokenized = map(lambda x: get_features(x, c_vectorizer, features_to_include), temp)
+    X = vect.fit_transform(tokenized)
+
+    clf = LogisticRegression(C=0.1, class_weight='auto', dual=False, fit_intercept=True,
+          intercept_scaling=1, penalty='l2', random_state=None, tol=0.0001)
+
+    clf.fit(X, y)
+
+    feature_names = vect.get_feature_names()
+    for i, class_label in enumerate([-1, 0, 1]):
+        top20 = np.argsort(clf.coef_[i])[-20:]
+        print("%s: %s" % (class_label,
+              "\t\n ".join(feature_names[j] for j in top20)))
+
+def test_unseen():
+    unseen_yaks = pd.read_csv("fsu_heldout_partitioned.csv", escapechar="\\", quotechar='"', error_bad_lines=False)
+
+    unseen_yaks.loc[unseen_yaks["score"] < 0, "score"] = -1
+    unseen_yaks.loc[(unseen_yaks["score"] > 0) &
+            (unseen_yaks["score"] < yaks["score"].quantile(.9)), "score"] = 0
+    unseen_yaks.loc[unseen_yaks["score"] >= yaks["score"].quantile(.9), "score"] = 1
+
+    discretize_scores()
+
+    # create feature matrix
+    temporal_features = ['time_of_day', 'day_of_week']
+    text_features = ['message_len', 'num_words', 'mean_word_len']
+    features_to_include = temporal_features + text_features
+    y = yaks["score"].values.astype(np.float32)
+    vect = DictVectorizer()
+    c_vectorizer = CountVectorizer(binary=True, ngram_range=(1,2)).build_analyzer()
+    temp = zip(*[list(yaks[x]) for x in ['message'] + features_to_include])
+    tokenized = map(lambda x: get_features(x, c_vectorizer, features_to_include), temp)
+    X = vect.fit_transform(tokenized)
+
+    clf = LogisticRegression(C=0.1, class_weight='auto', dual=False, fit_intercept=True,
+          intercept_scaling=1, penalty='l2', random_state=None, tol=0.0001)
+
+    clf.fit(X, y)
+
+    temp = zip(*[list(unseen_yaks[x]) for x in ['message'] + features_to_include])
+    tokenized = map(lambda x: get_features(x, c_vectorizer, features_to_include), temp)
+    X = vect.transform(tokenized)
+
+    predicted = clf.predict(X)
+    true = unseen_yaks.score
+
+    f1 = f1_score(true, predicted, average="weighted")
+
+    print "F1 score: %.5f" % f1
+
+
+
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) == 1:
+        main()
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'gridsearch':
+            grid_search()
+        if sys.argv[1] == 'features':
+            print_top_results()
+        if sys.argv[1] == 'test':
+            test_unseen()
